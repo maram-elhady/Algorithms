b96a359c7f742ad59e3b24948e72c5d115e81822
maram-elhady <maramayman738@gmail.com>
2025-08-14 17:26:25 +0300

    Recursion & BFS

---
+++ 03 Recursion & Divide-ans-conquer/RecursionBasics & Factorial/RecursionBasics & Factorial/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace RecursionBasics___Factorial
+{
+    internal class Program
+    {
+        // recursion = When a thing is defined in terms of itself. - Wikipedia
+        //      Apply the result of a procedure, to a procedure.  
+        //      A recursive method calls itself. Can be a substitute for iteration.
+        //      Divide a problem into sub-problems of the same type as the original.
+        //      Commonly used with advanced sorting algorithms and navigating trees
+
+        //iteration: repetation of internal process , recursion:repatation of a procedure
+
+        //      Advantages:
+        //      ----------
+        //      easier to read/write
+        //      easier to debug
+
+        //      Disadvantages:
+        //      ----------
+        //      sometimes slower
+        //      uses more memory
+        static void Main(string[] args)
+        {
+            walk(5);
+            Console.WriteLine(power(2, 8));
+            Console.WriteLine(factorial(7));
+        }
+
+        private static int power(int baseNum, int exponent)
+        {
+            if (exponent < 1) return 1; //base case
+            return baseNum * power(baseNum, exponent - 1 ); //recursive case
+        }
+
+        private static int factorial(int num)
+        {
+            if (num < 1) return 1; //base case
+            return num * factorial(num -1); //recursive case
+
+        }
+
+        //private static void walk(int steps)  //iteration
+        //{
+        //    for (int i = 0; i < steps; i++) 
+        //    {
+        //        Console.WriteLine("You take a step");
+        //    }
+        //}
+        private static void walk(int steps) //recursion
+        {
+            if(steps < 1) return; //baseCase
+            Console.WriteLine("You take a step!");
+            walk(steps - 1); //recursive case
+
+        }
+    }
+}
---
+++ 04 Graph/A_BFS/BFS/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Node
+{
+    public char Data { get; set; }
+
+    public Node(char data)
+    {
+        Data = data;
+    }
+}
+
+public class Graph
+{
+    private List<Node> nodes;
+    private int[,] matrix;
+
+    public Graph(int size)
+    {
+        nodes = new List<Node>();
+        matrix = new int[size, size];
+    }
+
+    public void AddNode(Node node)
+    {
+        nodes.Add(node);
+    }
+
+    public void AddEdge(int src, int dst)
+    {
+        matrix[src, dst] = 1;
+    }
+
+    public bool CheckEdge(int src, int dst)
+    {
+        return matrix[src, dst] == 1;
+    }
+
+    public void Print()
+    {
+        Console.Write("  ");
+        foreach (Node node in nodes)
+        {
+            Console.Write(node.Data + " ");
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.Write(nodes[i].Data + " ");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    public void BreadthFirstSearch(int src)
+    {
+        Queue<int> queue = new Queue<int>();
+        bool[] visited = new bool[matrix.GetLength(0)];
+
+        queue.Enqueue(src);
+        visited[src] = true;
+
+        while (queue.Count != 0)
+        {
+            src = queue.Dequeue();
+            Console.WriteLine(nodes[src].Data + " = visited");
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (matrix[src, i] == 1 && !visited[i])
+                {
+                    queue.Enqueue(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Breadth FS = Traverse a graph level by level
+        //              Utilizes a Queue
+        //              Better if destination is on average close to start
+        //              Siblings are visited before children
+
+        // Depth FS   = Traverse a graph branch by branch
+        //              Utilizes a Stack
+        //              Better if destination is on average far from the start
+        //              Children are visited before siblings
+        //              More popular for games/puzzles
+
+        Graph graph = new Graph(5);
+
+        graph.AddNode(new Node('A'));
+        graph.AddNode(new Node('B'));
+        graph.AddNode(new Node('C'));
+        graph.AddNode(new Node('D'));
+        graph.AddNode(new Node('E'));
+
+        graph.AddEdge(0, 1);
+        graph.AddEdge(1, 2);
+        graph.AddEdge(1, 4);
+        graph.AddEdge(2, 3);
+        graph.AddEdge(2, 4);
+        graph.AddEdge(4, 0);
+        graph.AddEdge(4, 2);
+
+        graph.Print();
+
+        graph.BreadthFirstSearch(2);
+    }
+}
